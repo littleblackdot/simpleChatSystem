15ab1775dff8d33215f93160923480a21939af90
littleBlackDot <2602690396@qq.com>
2021-07-21 08:22:48 -0700

    fileOperation_1

--- test.c
+++ test.c
@@ -12,17 +12,35 @@
 
 #include "dataParse.h"
 
+int getTotalUserNum(int fd);
+
+
 int main(){
     int fd = open("userInfo.txt", O_RDWR|O_CREAT, 0655);
-    char buffer1[20] = "total user: 1\n";
-    char buffer2[20] = "total user: 2\n";
+    char buffer1[30] = "total user: 112312\n";
+    char buffer2[30] = "total user: 2839021\n";
     lseek(fd, 0, SEEK_SET);
     write(fd, buffer1, strlen(buffer1));
-    sleep(5);
     lseek(fd, 0, SEEK_SET);
     write(fd, buffer2, strlen(buffer2));
     lseek(fd, 0, SEEK_END);
     write(fd, buffer1, strlen(buffer1));
+    printf("%d \n", getTotalUserNum(fd));
     close(fd);
     return 0;
 }
+
+int getTotalUserNum(int fd){
+    int ret = 0;
+    int index = 12;
+    char buffer[100];
+    memset(buffer, 0, sizeof(buffer));
+    lseek(fd, 0, SEEK_SET);
+    read(fd, buffer, sizeof(buffer));
+    while(buffer[index] != '\n'){
+        ret = ret * 10 + buffer[index]-'0';
+	index++;
+    }
+    return ret;
+}
+
