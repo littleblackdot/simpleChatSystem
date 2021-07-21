70e253ef96f7ebda6901cb20aa97dbbe6a88248a
littleblackdot <2602690396@qq.com>
2021-07-21 20:45:23 +0800

    for双机同步

---
+++ myType.h
@@ -0,0 +1,56 @@
+#ifndef __MYTYPE_H__
+#define __MYTYPE_H__
+
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
+typedef enum ActionType_Client{
+    Register, Login, Chat, broadCast, sendFile, showOnline, superOperation
+}ActionType_Client;
+
+typedef enum ActionType_Server{
+    resultReturn, sendFileRequest, messagePost
+}ActionType_Server;
+
+typedef enum StatusType{
+    normal, postBanend
+}StatusType;
+
+typedef enum UserType{
+    common, root
+}UserType;
+
+typedef struct USERINFO{
+    int id;
+    char name[40];
+    char pwd[20];
+}UserInfo_Client;
+
+typedef struct MSGTOSERVER{
+    ActionType_Client action;
+    int actionOption;
+    UserInfo_Client userInfo;
+    char message[1024];
+}msgToServer;
+
+
+typedef struct MSGTOCLIENT{
+    int result;
+    int reasonCode;
+    int nameNum;
+    ActionType_Server action;
+    char (*names)[40];
+    char message[1024];
+}msgToClient;
+
+
+typedef struct UserInfo_server{
+    struct sockaddr_in addr;
+    UserInfo_Client userInfo;
+    StatusType status;
+    UserType user;
+}UerInfo_server;
+
+
+
+#endif
